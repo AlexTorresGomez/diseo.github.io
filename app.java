ce89c5d5e0430eb2a0cb2490cad0233bf5c1649d
AlexTorresGomez <106146643+AlexTorresGomez@users.noreply.github.com>
2022-05-27 09:23:01 +0200

    Update app.java

---
+++ app.java
@@ -0,0 +1,13 @@
+public class app{
+
+public static void main(String[] args) {
+    System.out.println("Hello, World!");
+    System.out.println("Hola jon soy el Alex Torrs no el jordi ariza soy yo el origina spy yo el alex .t te lo juro");  
+    System.out.println("Adios Jon");  
+    System.out.println("hola soy german");
+    System.out.println("hola soy german");
+}
+
+
+
+}
